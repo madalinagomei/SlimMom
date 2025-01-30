77c9e5d5651a5ed848c212515a51edfae731567e
Madalina Gomei <madalinagomei@gmail.com>
2025-01-30 11:34:53 +0100

    small change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={{ marginTop: '100px' }}>
+    <footer
+      style={{
+        color: '#2f3c3c',
+        marginTop: '100px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       &copy; {new Date().getFullYear()} Slim Mom App created by{' '}
-      <em>Madalina Gomei</em>
+      <em> Madalina Gomei</em>
     </footer>
   );
 };
